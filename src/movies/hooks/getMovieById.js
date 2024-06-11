ca7fc80de43f08ff2c175798bebcd84560e1ea27
Rodrigo Fassi <rodrifassi7@gmail.com>
2024-06-11 16:45:39 -0300

    move details

--- src/movies/hooks/getMovieById.js
+++ src/movies/hooks/getMovieById.js
@@ -1,11 +1,12 @@
-
-import { useMemo } from 'react';
-import { useFetchMovies } from './useFetchMovies';
+import { useMemo } from "react";
+import { useFetchMovies } from "./useFetchMovies";
 
 export const useMovieById = (page, id) => {
   const { movies, loading, error } = useFetchMovies(page);
-  const movie = useMemo(() => movies.find(movie => movie.id === parseInt(id, 10)), [movies, id]);
+  const movie = useMemo(
+    () => movies.find((movie) => movie.id === parseInt(id, 10)),
+    [movies, id]
+  );
 
   return { movie, loading, error };
 };
- 
